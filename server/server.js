84e4c8de86edacad4d7e1b6884dd3e2d60c9244c
Carl Chen <carlchen214@gmail.com>
2016-07-15 20:57:24 -0700

    Make server serve static files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.listen(3000, function() {
   console.log('express is listening on port 3000');
